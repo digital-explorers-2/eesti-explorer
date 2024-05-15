c9d4daa348a64be53b4b5ac188868aa13e04b5ca
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-05-15 19:44:54 +0300

    feat(landing-page):creates hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
 import Navbar from "@/components/LandingPage/Navbar";
+import MediumButton from "@/components/MediumButton";
+import Image from "next/image";
+import HeroImage from "@/components/images/hero-image.png";
 
 
 export default async function Index() {
@@ -17,16 +20,25 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient();
 
   return (
-    <div className="">
+    <div>
       <Navbar/>
-
-      <div className="">
-        <main className="">
+      <div >
+        <main className="px-24 flex justify-between">
           {/* {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
-
+          <div id="words_left" className="w-[50%] pt-24">
+              <h1 className="text-6xl font-bold leading-normal"><span className="underline underline-offset-4 decoration-[#F57906]">Let's</span> Create A Memorable Journey</h1>
+              <div className="pr-10">
+                <p className="text-sm leading-loose pt-5">Eestie Explorer helps you discover magical places in Estonia and allows you to customize the places you would like to visit.</p>
+              </div>
+              <div className="pt-5">
+                <MediumButton >Learn more</MediumButton>
+              </div>
+          </div>
+          <div className="w-[45%]">
+            <Image src={HeroImage} alt="snow"/>
+          </div>
         </main>
       </div>
-
     </div>
   );
 }
