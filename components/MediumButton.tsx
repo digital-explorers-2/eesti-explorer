008f107c5292eb93d0946ad4725d81ed35321d64
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-05-15 19:44:54 +0300

    feat(landing-page):creates hero section

--- components/MediumButton.tsx
+++ components/MediumButton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MediumButton({children}:{children:React.ReactNode}) {
   return (
     <>
-        <button className="text-sm bg-[#F57906] px-7 py-2 rounded-[7px] text-white font-semibold">{children}</button>
+        <button className="text-sm bg-[#F57906] px-7 py-3 rounded-[7px] text-white font-semibold">{children}</button>
     </>
   )
 }
