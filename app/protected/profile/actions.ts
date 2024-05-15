ce22ea065037b7a75a3c9e4762a3520ce934252f
Crisnzano <crispus.nzano@gmail.com>
2024-05-15 17:24:57 +0300

    fix: fix actions file
    Added CRUD for actions file.

--- app/protected/profile/actions.ts
+++ app/protected/profile/actions.ts
@@ -1,68 +1,36 @@
-"use client"
+"use server"
 
-import { useState, useEffect } from "react";
-import { createClient } from "@/utils/supabase/client";
+import { createClient } from "@/utils/supabase/server";
 //import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 //import { Button } from "@/components/ui/button";
 
 const supabase = createClient();
 
-export function userEdit({change}: {change: any}) {
-    const [phone, setPhone] = useState(change.phone_number);
-    const [date, setDate] = useState(change.date_of_birth);
-  
-    const handleEdit = async () => {
-      const { error } = await supabase
-        .from("users")
-        .update({ phone, date })
-        .eq("id", change.user_id);
-  
-      if (error) {
-        console.error("Error updating record:", error.message);
-      } else {
-        console.log("Record updated successfully!");
-      
-      }
-    };
-
-/*
-    return (
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button className="bg-green-500">Edit</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <div style={{ marginBottom: "20px" }}>
-              <label>Title: {title ?? "NO title"}</label>
-            </div>
-            <div style={{ marginBottom: "20px" }}>
-              <label>
-                Priority
-                <input
-                  style={{ marginLeft: "5px" }}
-                  type="number"
-                  min="0"
-                  max="5"
-                  value={priority || 0}
-                  onChange={(e) => setPriority(parseInt(e.target.value))}
-                />
-              </label>
-            </div>
-            <div style={{ marginBottom: "20px" }}>
-              <label>
-                Done
-                <input
-                  style={{ marginLeft: "5px" }}
-                  type="checkbox"
-                  checked={done}
-                  onChange={(e) => setDone(e.target.checked)}
-                />
-              </label>
-            </div>
-            <Button className="bg-green-500" onClick={handleEdit}>
-              Save Changes
-            </Button>
-          </DialogContent>
-        </Dialog>
-      );*/
+export async function userDelete({ id }: { id: number }) {
+  const { error } = await supabase.from("users").delete().eq("id", id);
+  if (error) {
+    console.error("Error deleting record:", error.message);
+  } else {
+    console.log("Record deleted successfully!");
+  }
 }
+
+export async function userEdit({ id, phone, date }: { id: number, phone: string, date: string }) {
+    const { error } = await supabase.from("users").update({ phone_number:phone, date_of_birth:date }).eq("user_id", id);
+    if (error) {
+        console.error("Error updating record:", error.message);
+    } else {
+        console.log("Record updated successfully!");
+    }
+    }
+
+export async function userRead({ id }: { id: number }) {
+    const { data, error } = await supabase.from("users").select().eq("user_id", id);
+    if (error) {
+        console.error("Error fetching record:", error.message);
+    } else {
+        console.log("Record fetched successfully!");
+        return data;
+    }
+}
+    
