5cf408d557051971f4bf3355f171ea312b1d4fd8
Kigen29 <emmanuel.kemboi@strathmore.edu>
2024-05-19 22:56:15 +0300

    chore: update npm dependencies and add new UI components
    Update npm dependencies to their latest stable versions and add new UI components for the profile page.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,79 @@
+import * as React from "react"
 import Navbar from "../../components/LandingPage/Navbar";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 
-//navbar
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
-export default function Profile(){
-return(
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
 
-    <Navbar/>
-)
+export default function Profile() {
+    return (
+        <>
+            <Navbar />
+
+            <div className="">
+
+                <div className="flex justify-center items-center">
+
+                    <Card className="w-[350px]">
+                        <CardHeader>
+                            <CardTitle>Personal Information</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+
+
+                            <form >
+
+                                <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="name">Name</Label>
+                                        <Input id="name" placeholder="Name of your project" />
+                                    </div>
+                                </div>
+
+                                <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="name">Date of Birth:</Label>
+                                        <Input id="name" placeholder="Name of your project" />
+                                    </div>
+                                </div>
+
+                                <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="name">Phone Number:</Label>
+                                        <Input id="name" placeholder="Name of your project" />
+                                    </div>
+                                </div>
+
+                                <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="name">Location:</Label>
+                                        <Input id="name" placeholder="Name of your project" />
+                                    </div>
+                                </div>
+                            </form>
+
+                        </CardContent>
+                        <CardFooter className="flex justify-between">
+                            <Button variant="outline">Cancel</Button>
+                            <Button>Deploy</Button>
+                        </CardFooter>
+                    </Card>
+
+                </div>
+            </div>
+
+        </>
+
+    )
 
 }
