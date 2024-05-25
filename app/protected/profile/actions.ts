f3783c0fcfcc04c9d230e2a29a3c52b0f3a8a5cf
Crisnzano <crispus.nzano@gmail.com>
2024-05-25 14:10:37 +0300

    fix: change actions file for destinations

--- app/protected/profile/actions.ts
+++ app/protected/profile/actions.ts
@@ -1,8 +1,9 @@
-"use server"
+"use server";
 
 import { createClient } from "@/utils/supabase/server";
 //import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 //import { Button } from "@/components/ui/button";
+
 
 const supabase = createClient();
 
@@ -15,22 +16,37 @@ export async function userDelete({ id }: { id: number }) {
   }
 }
 
-export async function userEdit({ id, phone, date }: { id: number, phone: string, date: string }) {
-    const { error } = await supabase.from("users").update({ phone_number:phone, date_of_birth:date }).eq("user_id", id);
-    if (error) {
-        console.error("Error updating record:", error.message);
-    } else {
-        console.log("Record updated successfully!");
-    }
-    }
+export async function userEdit({
+  id,
+  phone,
+  date,
+}: {
+  id: number;
+  phone: string;
+  date: string;
+}) {
+  const { error } = await supabase
+    .from("users")
+    .update({ phone_number: phone, date_of_birth: date })
+    .eq("user_id", id);
+  if (error) {
+    console.error("Error updating record:", error.message);
+  } else {
+    console.log("Record updated successfully!");
+  }
+}
 
 export async function userRead({ id }: { id: number }) {
-    const { data, error } = await supabase.from("users").select().eq("user_id", id);
-    if (error) {
-        console.error("Error fetching record:", error.message);
-    } else {
-        console.log("Record fetched successfully!");
-        return data;
-    }
+  const { data, error } = await supabase
+    .from("users")
+    .select()
+    .eq("user_id", id);
+  if (error) {
+    console.error("Error fetching record:", error.message);
+  } else {
+    console.log("Record fetched successfully!");
+    return data;
+  }
 }
-    
+
+
