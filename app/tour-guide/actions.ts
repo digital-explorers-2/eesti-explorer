3ed1be53228bdbd20456045ab62b97bf1b87ceeb
nat254 <natalie.ndetei@strathmore.edu>
2024-06-05 20:33:10 +0300

    feat: add update functionality for the tour guide

--- app/tour-guide/actions.ts
+++ app/tour-guide/actions.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { createClient } from "@/utils/supabase/server"
+import { redirect } from "next/navigation"
 
 const supabase = createClient()
 
@@ -10,6 +11,21 @@ export async function guideRead() {
     console.error("Error fetching record:", error.message)
   } else {
     console.log("Record fetched successfully!")
+    return data
+  }
+}
+
+export async function updateCart(user_id: string, tour_guide_id: string) {
+  const { data, error } = await supabase
+    .from("cart")
+    .update({ tour_guide_id: tour_guide_id })
+    .eq("user_id", user_id)
+
+  if (error) {
+    console.error("Error updating record:", error.message)
+  } else {
+    console.log(data)
+    redirect("/billing")
     return data
   }
 }
