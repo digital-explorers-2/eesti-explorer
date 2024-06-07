4e08e08c55563e83d6a78d023e9b77ca409b00f4
Crisnzano <crispus.nzano@gmail.com>
2024-06-08 00:40:33 +0300

    fix: add stripe to payment page

---
+++ app/api/stripe/session/route.ts
@@ -0,0 +1,47 @@
+"use server"
+import { NextRequest, NextResponse } from "next/server"
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY || "", {
+  apiVersion: "2024-04-10",
+})
+
+export async function POST(req: NextRequest) {
+  const { amount } = await req.json()
+  const host = process.env.NEXT_PUBLIC_HOST
+
+  try {
+    const date = new Date().toISOString()
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "eur",
+            product_data: {
+              name: "INV-" + date,
+            },
+            unit_amount: amount * 100 || 100,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      cancel_url: `${host}`,
+      success_url: `${host}/success`,
+    })
+
+    return NextResponse.json({ sessionId: session.id })
+  } catch (err) {
+    console.error(err)
+    return NextResponse.json(
+      { error: "Error creating checkout session" },
+      { status: 500 },
+    )
+  }
+}
+
+export async function GET() {
+  return NextResponse.json({ error: "Method Not Allowed" }, { status: 405 })
+}
--- app/payments/page.tsx
+++ app/payments/page.tsx
@@ -1,20 +1,51 @@
 "use client"
 
-import Footer from "@/components/Footer"
+
+import CheckoutBtn from "@/components/Checkout";
+import React, { useEffect, useState, useRef } from "react"
+import Navbar from "@/components/Navbar"
+import Heading from "@/components/Heading"
+
+export default function PaymentPage() {
+const paymentFormRef = useRef(null)
+
+  return (
+    <>
+    <Navbar />
+    <div className="text-center mt-5">
+      <Heading
+        underlinedText="Payment"
+        otherText=" Details"
+      />
+    </div>
+    <div className="flex justify-center mt-8">
+      <form  method="post" id="payment-form" ref={paymentFormRef}>
+        <div className="form-row">
+          <div id="card-element"></div>
+          <div id="card-errors" role="alert"></div>
+        </div>
+        <CheckoutBtn/>
+      </form>
+    </div>
+  </>
+  );
+}
+
+
+{/*"use client"
+
 import Heading from "@/components/Heading"
 import Navbar from "@/components/Navbar"
-import React, { useEffect, useState } from "react"
-import Image from "next/image"
-import { BiWorld } from "react-icons/bi"
-import { RiMoneyEuroBoxLine } from "react-icons/ri"
-import { billingRead } from "./actions"
-import error from "next/error"
+import React, { useEffect, useState, useRef } from "react"
+import { billingRead, googlePayments } from "./actions"
 
 const PaymentPage = () => {
   const [billing, setBilling] = useState<any>(null)
+  const paymentFormRef = useRef(null)
 
   useEffect(() => {
     fetchBilling()
+    initializeGooglePayments()
   }, [])
 
   const fetchBilling = async () => {
@@ -23,6 +54,125 @@ const PaymentPage = () => {
       setBilling(billing)
     } catch (error) {
       console.error("Error fetching billing:", error)
+    }
+  }
+
+  const initializeGooglePayments = () => {
+    googlePayments()
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className="text-center mt-5">
+        <Heading
+          underlinedText="Payment"
+          otherText=" Details"
+        />
+      </div>
+      <div className="flex justify-center mt-8">
+        <script async src="https://pay.google.com/gp/p/js/pay.js"></script>
+        <form action=" " method="post" id="payment-form" ref={paymentFormRef}>
+          <div className="form-row">
+            <label htmlFor="card-element">Credit or debit card</label>
+            <div id="card-element"></div>
+            <div id="card-errors" role="alert"></div>
+          </div>
+          <button type="submit" className="text-white bg-orange-500 hover:bg-orange-600 font-bold py-2 px-4 rounded-lg flex items-center">
+            Buy with
+            <img
+              src="https://img.icons8.com/?size=100&id=Z5pgoU6ueRre&format=png&color=000000"
+              alt="Google Pay"
+              className="w-6 h-6 ml-2"
+              width= "20px"
+              height= "20px"
+            />
+            €{billing ? billing[0].amount : 0}
+          </button>
+        </form>
+      </div>
+    </>
+  )
+}
+export default PaymentPage */}
+
+
+
+{/*"use client"
+
+import { google } from "googleapis"
+import Heading from "@/components/Heading"
+import Navbar from "@/components/Navbar"
+import React, { useEffect, useState, useRef } from "react"
+import { billingRead } from "./actions"
+
+
+const PaymentPage = () => {
+  const [billing, setBilling] = useState<any>(null)
+  const paymentFormRef = useRef(null)
+
+  useEffect(() => {
+    fetchBilling()
+    initializeGooglePayments()
+  }, [])
+
+  const fetchBilling = async () => {
+    try {
+      const billing = await billingRead({ id: 1 })
+      setBilling(billing)
+    } catch (error) {
+      console.error("Error fetching billing:", error)
+    }
+  }
+
+  const initializeGooglePayments = async () => {
+    try {
+      const response = await fetch("/api/google-pay")
+      const googlePaymentRequest = await response.json()
+
+      const paymentsClient = new google.payments.api.PaymentsClient({
+        environment: "TEST",
+      })
+
+      const isReadyToPayRequest = {
+        apiVersion: 2,
+        apiVersionMinor: 0,
+        allowedPaymentMethods: googlePaymentRequest.allowedPaymentMethods,
+      }
+
+      paymentsClient
+        .isReadyToPay(isReadyToPayRequest)
+        .then(function (response: any) {
+          if (response.result) {
+            // add a Google Pay payment button
+            const button = paymentsClient.createButton({
+              onClick: () => {
+                paymentsClient
+                  .loadPaymentData(googlePaymentRequest)
+                  .then(function (paymentData: any) {
+                    // if using gateway tokenization, pass this token without modification
+                    const paymentToken =
+                      paymentData.paymentMethodData.tokenizationData.token
+                    console.log("Payment Token:", paymentToken)
+                  })
+                  .catch(function (err: any) {
+                    // show error in developer console for debugging
+                    console.error(err)
+                  })
+              },
+              allowedPaymentMethods: googlePaymentRequest.allowedPaymentMethods,
+            })
+            if (paymentFormRef.current) {
+              ;(paymentFormRef.current as HTMLFormElement).appendChild(button)
+            }
+          }
+        })
+        .catch(function (err: any) {
+          // show error in developer console for debugging
+          console.error(err)
+        })
+    } catch (error) {
+      console.error("Error initializing Google Payments:", error)
     }
   }
 
@@ -36,10 +186,14 @@ const PaymentPage = () => {
         />
       </div>
       <div className="flex justify-center mt-8">
+        <script
+          async
+          src="https://pay.google.com/gp/p/js/pay.js"></script>
         <form
-          action="/charge"
+          action=""
           method="post"
-          id="payment-form">
+          id="payment-form"
+          ref={paymentFormRef}>
           <div className="form-row">
             <label htmlFor="card-element">Credit or debit card</label>
             <div id="card-element"></div>
@@ -48,19 +202,23 @@ const PaymentPage = () => {
               role="alert"></div>
           </div>
           <button
-           className="text-white bg-orange-500 hover:bg-orange-600 font-bold py-2 px-4 rounded-lg flex items-center">Submit Payment</button>
+            type="submit"
+            className="text-white bg-orange-500 hover:bg-orange-600 font-bold py-2 px-4 rounded-lg flex items-center">
+            Submit Payment
+          </button>
         </form>
       </div>
       <div className="flex justify-center mt-8">
-        <button
-          className="text-white bg-orange-500 hover:bg-orange-600 font-bold py-2 px-4 rounded-lg flex items-center">
-          Pay €{billing?.amount}
+        <button className="text-white bg-orange-500 hover:bg-orange-600 font-bold py-2 px-4 rounded-lg flex items-center">
+          Pay €{billing ? billing[0].amount : 0}
         </button>
       </div>
-
-      <Footer />
     </>
   )
-
 }
-export default PaymentPage
+
+export default PaymentPage*/}
+
+
+
+
