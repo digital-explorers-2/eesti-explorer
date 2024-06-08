73b6aebf5f75301faa40eb175370cf44b7fa6a28
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-06-08 14:51:03 +0300

    feat:(admin-dashboard-backend)
    integrates backend routes with front end, fetches data for grouphs and counts as well as top guides

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,6 +6,7 @@ import TopNav from "@/components/Admin/TopNav"
 import TopTourGuides from "@/components/Admin/TopTourGuides"
 
 export default function page() {
+
   return (
     <div className="flex">
       <SideBar />
@@ -13,7 +14,7 @@ export default function page() {
         id="main"
         className="rounded-3xl -ml-5 bg-white w-full pl-5">
         <TopNav />
-        <TopCards />
+        <TopCards/>
         <MainCharts />
         <TopTourGuides />
       </div>
