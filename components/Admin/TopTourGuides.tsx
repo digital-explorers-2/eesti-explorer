db89318dc84b49355ae8f432bdf58a4c4d98a6b8
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-06-08 14:51:03 +0300

    feat:(admin-dashboard-backend)
    integrates backend routes with front end, fetches data for grouphs and counts as well as top guides

--- components/Admin/TopTourGuides.tsx
+++ components/Admin/TopTourGuides.tsx
@@ -1,23 +1,24 @@
+import {useEffect, useState} from "react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-
-const tourGuides = [
-  {
-    name: "Nicholas Patrick",
-    bookings: 80,
-    price: "5 Euros/hr",
-    email: "nicola@gmail.com",
-    image: "https://avatars.githubusercontent.com/u/1291?v=4",
-  },
-  {
-    name: "Derrick Spencer",
-    bookings: 40,
-    price: "20 Euros/hr",
-    email: "derick@gmail.com",
-    image: "https://avatars.githubusercontent.com/u/12423?v=4",
-  },
-]
+import { topTourGuides } from "@/app/admin/action"
 
 export default function TopTourGuides() {
+  const [tourGuides, setTourGuides] = useState<any[]>([])
+  useEffect(()=>{
+    const fetchCount = async () =>{
+      try{
+        const response = await topTourGuides();
+        if(response){
+          setTourGuides(response)
+        }
+      }
+      catch(error){
+        console.log("Could not fetch counts ", error)
+      }
+    }
+    fetchCount();
+  },[])
+
   return (
     <div
       id="tour-guides-section"
@@ -25,20 +26,20 @@ export default function TopTourGuides() {
       <h1 className="text-sm font-bold">Top tour guides</h1>
       <div className="mt-3 flex flex-col gap-4">
         {tourGuides.map(tourGuide => (
-          <div className="border-[1.3px] rounded-lg px-7 py-2  border-[#D3CBFB] w-[96%] flex gap-20 align-middle justify-center">
+          <div key={tourGuide.tourGuides_id} className="border-[1.3px] rounded-lg px-7 py-2  border-[#D3CBFB] w-[96%] flex gap-20 align-middle justify-center">
             <div>
               <Avatar className="w-7 h-7">
                 <AvatarImage
-                  src={tourGuide.image}
+                  src={tourGuide.image_path}
                   alt="@shadcn"
                 />
-                <AvatarFallback>AK</AvatarFallback>
+                <AvatarFallback>{tourGuide.firstname}</AvatarFallback>
               </Avatar>
             </div>
             <div className="flex mt-1 gap-20">
-              <p className="text-[#797D8C] text-sm">{tourGuide.name}</p>
-              <p className="text-sm font-bold">{tourGuide.bookings}</p>
-              <p className="text-[#797D8C] text-sm">{tourGuide.price}</p>
+              <p className="text-[#797D8C] text-sm">{tourGuide.firstname} {tourGuide.lastname}</p>
+              <p className="text-sm font-bold">rating: {tourGuide.rating}</p>
+              <p className="text-[#797D8C] text-sm">{tourGuide.fee} euros/hour</p>
               <p className="text-[#797D8C] text-sm">{tourGuide.email}</p>
             </div>
           </div>
