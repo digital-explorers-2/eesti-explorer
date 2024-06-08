6ea6672e2ea55937eb83206ce0bf38c08f64454c
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-06-08 18:18:48 +0300

    fix:reverted to old next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = {}
+
+module.exports = {
+  nextConfig,
   images: {
     domains: ["kuvdmztnxdacvqvwccwm.supabase.co"], // Add your Supabase storage hostname here
   },
-  webpack: (config, { isServer, dev }) => {
-    if (dev) {
-      config.cache = false; // Disable Webpack caching in development
-    } else {
-      config.cache = {
-        type: 'filesystem',
-        buildDependencies: {
-          config: [__filename],
-        },
-        store: 'pack',
-        cacheLocation: 'node_modules/.cache/webpack',
-        managedPaths: [/^(.+?[\\/]node_modules[\\/])/],
-      };
-    }
-
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+}
