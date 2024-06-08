f8969638de839ffa262fcb14164bbef5a1659373
nat254 <natalie.ndetei@strathmore.edu>
2024-06-08 15:56:05 +0300

    feat: add UI for guides page
    Additional edit button

---
+++ components/EditButton.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function MediumButton({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode
+  onClick?: () => void
+}) {
+  return (
+    <>
+      <button
+        onClick={onClick}
+        className="text-sm bg-[#43BE83] px-5 py-1 rounded-[10px] text-white font-semibold"
+      >
+        {children}
+      </button>
+    </>
+  )
+}
