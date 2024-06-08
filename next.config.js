8b1312a080ca51469ca0477df71d0b1e324082d3
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-06-08 18:09:58 +0300

    fix(vercel-error):changes next.config.js and also adds route for admin/users

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = {
-  nextConfig,
+const nextConfig = {
   images: {
     domains: ["kuvdmztnxdacvqvwccwm.supabase.co"], // Add your Supabase storage hostname here
   },
-}
+  webpack: (config, { isServer, dev }) => {
+    if (dev) {
+      config.cache = false; // Disable Webpack caching in development
+    } else {
+      config.cache = {
+        type: 'filesystem',
+        buildDependencies: {
+          config: [__filename],
+        },
+        store: 'pack',
+        cacheLocation: 'node_modules/.cache/webpack',
+        managedPaths: [/^(.+?[\\/]node_modules[\\/])/],
+      };
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
