79fa5b4ecdd406605e56f0e0ebeee708c0eb6c30
Kigen29 <emmanuel.kemboi@strathmore.edu>
2024-06-09 16:43:21 +0300

    feat: add read and delete functionalities to admin-users

--- components/Admin/users/Users.tsx
+++ components/Admin/users/Users.tsx
@@ -1,58 +1,68 @@
+"use client"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import DeleteButton from "@/components/DeleteButton"
-
-const tourGuides = [
-  {
-    name: "Nicholas Patrick",
-    bookings: 80,
-    price: "5 Euros/hr",
-    email: "nicola@gmail.com",
-    image: "https://avatars.githubusercontent.com/u/1291?v=4",
-  },
-  {
-    name: "Derrick Spencer",
-    bookings: 40,
-    price: "20 Euros/hr",
-    email: "derick@gmail.com",
-    image: "https://avatars.githubusercontent.com/u/12423?v=4",
-  },
-]
+import EditButton from "@/components/EditButton"
+import React, { useEffect, useState } from "react"
+import { deleteUser, readUsers } from "@/app/admin/users/actions"
 
 export default function users() {
+  const [users, setUsers] = useState<any>([])
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const userData = await readUsers()
+        if (userData) {
+          setUsers(userData.users)
+        }
+      } catch (error) {
+        console.log("Error fetching users:", error)
+      }
+    }
+    fetchUser()
+  }, [])
+
+  console.log("Users:", users) // Debugging: Log users state
+
   return (
-    <div
-      id="tour-guides-section"
-      className="mx-5">
-      <div className="mt-3 flex flex-col gap-4">
-        {tourGuides.map(tourGuide => (
-          <div className="border-[1.3px] rounded-lg px-7 py-2  justify-between border-[#D3CBFB] w-[96%] flex gap-15 align-middle justify-center">
-            <div>
-              <Avatar className="w-7 h-7">
-                <AvatarImage
-                  src={tourGuide.image}
-                  alt="@shadcn"
-                />
-                <AvatarFallback>AK</AvatarFallback>
-              </Avatar>
-            </div>
-            <div className="flex mt-1 gap-20">
-              <p className="text-[#797D8C] text-sm">{tourGuide.name}</p>
-              <p className="text-[#797D8C] text-sm">{tourGuide.email}</p>
-            </div>
-            <div className="flex justify-center gap-2">
-            <div>
-                <DeleteButton>Delete</DeleteButton>
-            </div>
-
-            <div>
-                <DeleteButton>Delete</DeleteButton>
-            </div>
-            </div>
-
-
+    <>
+      {users && users.length > 0 ? (
+        <div
+          id="tour-guides-section"
+          className="mx-5 mt-12">
+          <div className="mt-3 flex flex-col gap-5">
+            {users.map((users: any) => (
+              <div className="border-[1.3px] rounded-lg px-7 py-2  justify-between border-[#D3CBFB] w-[96%] flex gap-20 align-middle ">
+                <div className="flex my-2">
+                  <Avatar className="w-7 h-7">
+                    <AvatarImage
+                      src={users.user_metadata?.picture}
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>AK</AvatarFallback>
+                  </Avatar>
+                </div>
+                <div className="flex my-3 gap-20">
+                  <p className="text-[#797D8C] text-sm">
+                    {users.user_metadata?.full_name}
+                  </p>
+                  <p className="text-[#797D8C] text-sm">
+                    {users.user_metadata?.email}
+                  </p>
+                </div>
+                <div className="flex justify-center gap-4 ">
+                  <EditButton>Edit</EditButton>
+                  <DeleteButton onClick={() => deleteUser(users.id)}>Delete</DeleteButton>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      ) : (
+        <div className="flex justify-center items-center h-screen">
+          <p>Loading...</p>
+        </div>
+      )}
+    </>
   )
 }
