5b5ece2558434714dce79a3212aff5309c32359d
Kigen29 <emmanuel.kemboi@strathmore.edu>
2024-06-11 20:37:13 +0300

    fix: add grandtotal to the checkout button

--- app/billing/page.tsx
+++ app/billing/page.tsx
@@ -1,90 +1,90 @@
 "use client"
 import Navbar from "@/components/Navbar"
 import Heading from "@/components/Heading"
-import LargeButton from "@/components/LargeButton"
 import { useEffect, useState } from "react"
 import { readBilling } from "./actions"
 import { createClient } from "@/utils/supabase/client"
+import CheckoutBtn from "@/components/Checkout";
 import { useRouter } from "next/navigation"
 
 export default function Billing() {
     const [destinationFee, setDestinationFee] = useState<number>(0)
     const [tourGuideFee, setTourGuideFee] = useState<any>(0)
-    const [numPeople, setNumPeople] = useState<number>(1) // State to keep track of the number of people
+    const [numPeople, setNumPeople] = useState<number>(1) 
     const serviceFee = 5
     const grandTotal = destinationFee + tourGuideFee + serviceFee
-    const router = useRouter()  
+    const router = useRouter() 
 
-    const billingData = [
-      {
-        fee_type: "Tour Guide Fees",
-        fee_description: "Includes entry fee charges, and activity charges",
-        fee_cost: tourGuideFee + " euros",
-      },
-      {
-        fee_type: "Service Fees",
-        fee_description: "Includes entry fee charges, and activity charges",
-        fee_cost: serviceFee + " euros",
-      },
-    ]
+  const billingData = [
+    {
+      fee_type: "Tour Guide Fees",
+      fee_description: "Includes entry fee charges, and activity charges",
+      fee_cost: tourGuideFee + " euros",
+    },
+    {
+      fee_type: "Service Fees",
+      fee_description: "Includes entry fee charges, and activity charges",
+      fee_cost: serviceFee + " euros",
+    },
+  ]
 
-    const destinationData =[
-      {
-        fee_type: "Destination Fees",
-        fee_description: "Includes entry fee charges, and activity charges",
-        fee_cost: (destinationFee * numPeople) + " euros", // Adjusted by number of people
-      }
-    ]
+  const destinationData =[
+    {
+      fee_type: "Destination Fees",
+      fee_description: "Includes entry fee charges, and activity charges",
+      fee_cost: (destinationFee * numPeople) + " euros", // Adjusted by number of people
+    }
+  ]
 
     const supabase = createClient()
 
-    useEffect(() => {
-      const fetchUser = async () => {
-        const {
-          data: { user },
-        } = await supabase.auth.getUser()
+useEffect(() => {
+  const fetchUser = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
 
-        if (!user) {
-          return router.replace("/login")
-        }
-      }
-      fetchUser()
-
-      const handleReadBilling = async () => {
-            try {
-                const { data, error } = await supabase.auth.getUser()
-                if (error) {
-                    throw new Error(error.message)
-                }
-                const response = await readBilling(data.user.id)
-                if(response){
-                   setDestinationFee(response.sum)
-                   setTourGuideFee(response.guide)
-                }
-            } catch (error) {
-                console.log("Error fetching billing")
-            }
-        }
-        handleReadBilling()
-    }, [])
-
-    const handleNumPeopleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const value = parseInt(event.target.value, 10)
-      setNumPeople(isNaN(value) || value < 1 ? 1 : value > 10 ? 10  : value)
+    if (!user) {
+      return router.replace("/login")
     }
+  }
+  fetchUser()
+
+    const handleReadBilling = async () => {
+        try {
+            const { data, error } = await supabase.auth.getUser()
+            if (error) {
+                throw new Error(error.message)
+            }
+            const response = await readBilling(data.user.id)
+            if(response){
+               setDestinationFee(response.sum)
+               setTourGuideFee(response.guide)
+            }
+        } catch (error) {
+            console.log("Error fetching billing")
+        }
+    }
+    handleReadBilling()
+}, [])
+
+const handleNumPeopleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const value = parseInt(event.target.value, 10)
+  setNumPeople(isNaN(value) || value < 1 ? 1 : value > 10 ? 10 : value)
+}
 
     return (
       <>
         <Navbar />
 
-        <div className="text-center">
-          <Heading
-            underlinedText="Billing"
-            otherText=" Summary"
-          />
-        </div>
+      <div className="text-center">
+        <Heading
+          underlinedText="Billing"
+          otherText=" Summary"
+        />
+      </div>
 
-        <div className="mx-72 pt-20 flex flex-col gap-10 border-b-4 pb-16">
+      <div className="mx-72 pt-20 flex flex-col gap-10 border-b-4 pb-16">
           {destinationData.map(data => (
             <div className="flex justify-between" key={data.fee_type}>
               <div className="">
@@ -108,22 +108,22 @@ export default function Billing() {
             </div>
           ))}
         </div>
-          
-        <div className="mx-72 pt-20 flex flex-col gap-10 border-b-4 pb-16">
-          {billingData.map(data => (
-            <div className=" flex justify-between" key={data.fee_type}>
-              <div className="">
-                <h1 className="text-lg font-semibold">{data.fee_type}</h1>
-                <p className=" text-sm pt-5 text-gray-500">
-                  {data.fee_description}{" "}
-                </p>
-              </div>
-              <div>
-                <h1 className="font-bold text-xl pt-2">{data.fee_cost}</h1>
-              </div>
+        
+      <div className="mx-72 pt-20 flex flex-col gap-10 border-b-4 pb-16">
+        {billingData.map(data => (
+          <div className=" flex justify-between">
+            <div className="">
+              <h1 className="text-lg font-semibold">{data.fee_type}</h1>
+              <p className=" text-sm pt-5 text-gray-500">
+                {data.fee_description}{" "}
+              </p>
             </div>
-          ))}
-        </div>
+            <div>
+              <h1 className="font-bold text-xl pt-2">{data.fee_cost}</h1>
+            </div>
+          </div>
+        ))}
+      </div>
 
         <div className="flex justify-between mx-72 pt-10 pb-10 ">
           <div>
